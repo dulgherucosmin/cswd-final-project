b5cf041f40fd1a79e9dbb08d95a7404b5ec84181
kisa <kisandudk@Gmail.com>
2025-05-14 16:28:36 +0100

    feat: update gallery filtering functionality and improve image categorization

--- CWD_Ass4_3161052_3142847_3161763/src/scripts/gallery.js
+++ CWD_Ass4_3161052_3142847_3161763/src/scripts/gallery.js
@@ -6,3 +6,28 @@ if (gallery) {
         selector: 'a'
     });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const imageBoxes = document.querySelectorAll('.image-box');
+
+    document.querySelector('[data-filter="all"]').classList.add('active-filter');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            filterButtons.forEach(btn => btn.classList.remove('active-filter'));
+
+            button.classList.add('active-filter');
+
+            const filterValue = button.getAttribute('data-filter');
+            
+            imageBoxes.forEach(box => {
+                if (filterValue === 'all' || box.getAttribute('data-name') === filterValue) {
+                    box.style.display = 'block';
+                } else {
+                    box.style.display = 'none';
+                }
+            });
+        });
+    });
+});
